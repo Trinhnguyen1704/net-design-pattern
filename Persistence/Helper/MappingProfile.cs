6db8a012aa57e7ba2e7050d6fe60392a824563b0
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-04 15:26:31 +0700

    add helper persistence

---
+++ Persistence/Helper/MappingProfile.cs
@@ -0,0 +1,16 @@
+using System;
+using AutoMapper;
+using net_design_pattern.Domain.Models;
+using net_design_pattern.Domain.Models.DTOs;
+
+namespace net_design_pattern.Persistence.Helper
+{
+    public class MappingProfile : AutoMapper.Profile
+    {
+        public MappingProfile()
+        {
+
+        } 
+            
+    }
+}
