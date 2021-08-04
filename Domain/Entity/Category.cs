61ea6f59f99f67eceebd799235585e382e9e1858
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-04 09:22:39 +0700

    add category entity

---
+++ Domain/Entity/Category.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using net_design_pattern.Persistence.BaseEntity;
+
+namespace net_design_pattern.Domain.Models
+{
+    [Table("Category")]
+    public class Category : BaseEntity
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+        [Key]
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public virtual ICollection<Product> Products {get; set;}
+    }
+}
