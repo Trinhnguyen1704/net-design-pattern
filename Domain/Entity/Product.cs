2df35546ae4313dac915d0108556f3ce425b4208
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-04 09:23:41 +0700

    add product  entity

---
+++ Domain/Entity/Product.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using net_design_pattern.Persistence.BaseEntity;
+
+namespace net_design_pattern.Domain.Models
+{
+    [Table("Product")]
+    public class Product : BaseEntity
+    {
+        [Key]
+        public int Id {get; set;}
+        [Required][MaxLength(320)]
+        public string Name {get; set;}
+        public float Price {get; set;}
+        public int NumInStock {get; set;}
+        public string Description {get; set;}
+        public bool IsAvailable {get; set;}
+        public int CategoryId {get; set;}
+        [ForeignKey("CategoryId")]
+        public virtual Category Category {get; set;}
+
+    }
+}
