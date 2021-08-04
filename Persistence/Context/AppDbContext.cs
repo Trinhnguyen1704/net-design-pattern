e284e2f2f88c56a89c8a987f3ca9f5133af34729
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-04 10:02:08 +0700

    add database context

---
+++ Persistence/Context/AppDbContext.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using net_design_pattern.Domain.Models;
+
+namespace net_design_pattern.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+        public DbSet<Account> Accounts {get; set;}
+        public DbSet<Profile> Profiles {get; set;}
+        public DbSet<Role> Roles {get; set;}
+        public DbSet<Product> Products {get; set;}
+        public DbSet<Category> Categories {get; set;}
+        public DbSet<Order> Orders {get; set;}
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Account>()
+            .HasMany(x => x.Roles)
+            .WithMany(x => x.Accounts);
+
+            builder.Entity<Account>()
+            .HasMany(x => x.Orders)
+            .WithOne(x => x.Account);
+
+            builder.Entity<Account>()
+            .HasOne(x => x.Profile)
+            .WithOne(x => x.Account);
+
+            builder.Entity<Product>()
+            .HasOne(x => x.Category)
+            .WithMany(x => x.Products);
+
+            builder.Entity<Product>()
+            .HasMany(x => x.Orders)
+            .WithMany(x => x.Products);
+            
+        }
+    }
+}
