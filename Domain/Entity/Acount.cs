7b0dce211ee8977bf5d5ab16e984e3dfe2e44334
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-04 08:14:24 +0700

    init entity

---
+++ Domain/Entity/Acount.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_design_pattern.Domain.Models
+{
+    public class Acount
+    {
+        
+    }
+}
