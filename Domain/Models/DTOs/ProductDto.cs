21e12e42172799100e259d618692fb8e1574691f
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-05 06:35:39 +0700

    add product dto

---
+++ Domain/Models/DTOs/ProductDto.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_design_pattern.Domain.Models.DTOs
+{
+    public class ProductDto
+    {
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public float Price {get; set;}
+        public int NumInStock {get; set;}
+        public string Description {get; set;}
+        public string Status {get; set;}
+        public int CategoryId {get; set;}
+        public Category category {get; set;}
+
+    }
+}
