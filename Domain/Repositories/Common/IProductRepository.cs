940672103a8dcab09dcf2f76ad4cdf6b7b4e77de
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-05 06:36:41 +0700

    add common repository for mutual using

---
+++ Domain/Repositories/Common/IProductRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using net_design_pattern.Domain.Models;
+
+namespace net_design_pattern.Domain.Repositories.Common
+{
+    public interface IProductRepository
+    {
+        List<Product> GetProducts();
+        Product GetProductById(int productId);
+        Product UpdateProduct(int productId, Product product);
+        Product AddProduct(Product product);
+        bool DeleteProduct(int productId);
+    }
+}
