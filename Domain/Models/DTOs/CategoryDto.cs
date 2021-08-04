f948c871f5f6664fe749cbd365fab0c3e8916fee
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-05 06:35:07 +0700

    add category dto

---
+++ Domain/Models/DTOs/CategoryDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_design_pattern.Domain.Models.DTOs
+{
+    public class CategoryDto
+    {
+        public int Id {get; set;}
+        public string Name {get; set;}
+    }
+}
