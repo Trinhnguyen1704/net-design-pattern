73cb24d8527ac2868500ec2b333c6e480e3441aa
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-05 08:12:25 +0700

    add product service

---
+++ Services/Common/ProductService.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using net_design_pattern.Domain.Models.DTOs;
+using net_design_pattern.Domain.Repositories.Authorization;
+using net_design_pattern.Domain.Repositories.Common;
+using net_design_pattern.Domain.Services.Common;
+
+namespace  net_design_pattern.Services.Common
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IRoleRepository _roleRepository;
+        private readonly IMapper _mapper;
+        public ProductService(IProductRepository productRepository, IRoleRepository roleRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _roleRepository = roleRepository;
+            _mapper = mapper;
+        }
+        public ProductDto AddProduct(int accountId, ProductDto Product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteProduct(int accountId, int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductDto GetProductById(int accountId, int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductDto> GetProducts(int accountId)
+        {
+            var products = _productRepository.GetProducts();
+            var listProductRet = _mapper.Map<List<ProductDto>>(products);
+            for (int i = 0; i < listProductRet.Count; i++)
+            {
+                listProductRet[i].Category = _mapper.Map<CategoryDto>(products[i].Category);
+            }
+            return listProductRet;
+        }
+
+        public ProductDto UpdateProduct(int accountId, int productId, ProductDto product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
