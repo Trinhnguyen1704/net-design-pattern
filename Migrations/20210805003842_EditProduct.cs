04d5b1b5e6aa97e38815035907b3bbfea74444ff
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-05 08:14:03 +0700

    add migration update table product

---
+++ Migrations/20210805003842_EditProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace net_design_pattern.Migrations
+{
+    public partial class EditProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "IsAvailable",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsAvailable",
+                table: "Product",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
