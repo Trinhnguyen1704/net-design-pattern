78cc11af2ac219bba35ad68b747d31c5d40da9eb
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-05 14:56:12 +0700

    add get category function

--- Persistence/Repositories/Admin/CategoryRepository.cs
+++ Persistence/Repositories/Admin/CategoryRepository.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using net_design_pattern.Domain.Models;
 using net_design_pattern.Domain.Repositories.Admin;
+using net_design_pattern.Persistence.Context;
 
 namespace net_design_pattern.Persistence.Repositories.Admin
 {
     public class CategoryRepository : ICategoryRepository
     {
+        private readonly AppDbContext _context;
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
         public Product AddCategory(Category category)
         {
             throw new NotImplementedException();
@@ -21,12 +27,32 @@ namespace net_design_pattern.Persistence.Repositories.Admin
 
         public List<Category> GetCategories()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _context.Categories
+                .Where(x => x.IsDeleted == false)
+                .OrderBy(x => x.Id)
+                .ToList();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
         }
 
         public Category GetCategoryById(int categoryId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var category = _context.Categories.Where(x => x.IsDeleted == false).FirstOrDefault(x => x.Id == categoryId);
+                return category;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
         }
 
         public Category UpdateCategory(int categoryId, Category category)
--- Services/Admin/CategoryService.cs
+++ Services/Admin/CategoryService.cs
@@ -2,14 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using net_design_pattern.Domain.Models;
 using net_design_pattern.Domain.Models.DTOs;
+using net_design_pattern.Domain.Repositories.Admin;
+using net_design_pattern.Domain.Repositories.Authorization;
 using net_design_pattern.Domain.Services.Admin;
 
 namespace net_design_pattern.Services.Admin
 {
     public class CategoryService : ICategoryService
     {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IRoleRepository _roleRepository;
+        private readonly IMapper _mapper;
+        public CategoryService(ICategoryRepository categoryRepository, IRoleRepository roleRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _roleRepository = roleRepository;
+            _mapper = mapper;
+        }
+
         public CategoryDto AddCategory(int accountId, CategoryDto category)
         {
             throw new NotImplementedException();
@@ -20,14 +33,33 @@ namespace net_design_pattern.Services.Admin
             throw new NotImplementedException();
         }
 
-        public List<Category> GetCategories(int accountId)
+        public List<CategoryDto> GetCategories(int accountId)
         {
-            throw new NotImplementedException();
+            var checkRole = _roleRepository.CheckRole(accountId);
+            if (!checkRole)
+            {
+                return null;
+            }
+            var listCategories = _categoryRepository.GetCategories();
+            var listCategoryRes = _mapper.Map<List<CategoryDto>>(listCategories);
+    
+            return listCategoryRes;
         }
 
-        public Category GetCategoryById(int accountId, int categoryId)
+        public CategoryDto GetCategoryById(int accountId, int categoryId)
         {
-            throw new NotImplementedException();
+            var checkRole = _roleRepository.CheckRole(accountId);
+            if (!checkRole)
+            {
+                return null;
+            }
+            var category = _categoryRepository.GetCategoryById(categoryId);
+            if (category == null)
+            {
+                return null;
+            }
+            var categoryRes = _mapper.Map<CategoryDto>(category);
+            return categoryRes;
         }
 
         public CategoryDto UpdateCategory(int accountId, int categoryId, CategoryDto category)
