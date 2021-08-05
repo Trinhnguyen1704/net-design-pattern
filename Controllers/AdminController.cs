ce1fb9ea1773e412302bf4f7130735c0fc0c57f6
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-05 17:05:25 +0700

    apply base  reponse class for reponse data

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using net_design_pattern.Domain.Models.DTOs;
 using net_design_pattern.Domain.Repositories.Authorization;
 using net_design_pattern.Domain.Services.Admin;
+using net_design_pattern.Domain.Services.Communication;
 
 namespace net_design_pattern.Controllers
 {
@@ -30,56 +31,96 @@ namespace net_design_pattern.Controllers
         //     return _roleRepository.GetRoles(accountId);
         // }
         [HttpGet("category")]
-        public ActionResult<IEnumerable<CategoryDto>> GetCategories()
+        public ActionResult GetCategories()
         {
-            // accountId is get from login, but now is hard data
             var accountId = 2;
-            return (_categoryService.GetCategories(accountId));
+            var response = new Response<List<CategoryDto>>();
+
+            var categories = _categoryService.GetCategories(accountId);
+            if(categories == null)
+            {
+                //response.Code = 404; // Bad request error code
+                response.IsSuccess = false;
+                response.Message = "User don't have permission.";
+                return BadRequest(response);
+            }
+            response.Data = categories;
+            response.Message = "Get category list successfully";
+            return Ok(response);
         }
 
         [HttpGet("category/{categoryId}")]
-        public ActionResult<CategoryDto> GetCategory(int categoryId)
+        public ActionResult GetCategory(int categoryId)
         {
-            // accountId is get from login, but now is hard data
-            // 
+            var response = new Response<CategoryDto>();
             var accountId = 2;
-            return _categoryService.GetCategoryById(accountId, categoryId);
+            var category =  _categoryService.GetCategoryById(accountId, categoryId);
+            if(category == null)
+            {
+                //response.Code = 404; // Bad request error code
+                response.IsSuccess = false;
+                response.Message = "Category is not exist.";
+                return NotFound(response);
+            }
+            response.Data = category;
+            response.Message = "Get category successfully";
+            return Ok(response);
         }
 
         [HttpPost("category")]
-        public ActionResult<CategoryDto> AddCategory([FromBody] CategoryDto category)
+        public ActionResult AddCategory([FromBody] CategoryDto category)
         {
             var accountId = 2;// Only admin have permission to add product
+            var response = new Response<CategoryDto>();
             var categoryRes = _categoryService.AddCategory(accountId, category);
 
             if (categoryRes == null)
             {
-                return null;
+                response.Code = 401;
+                response.IsSuccess = false;
+                response.Message = "User don't have permission.";
+                return Unauthorized(response);
             }
-            return categoryRes;
+            response.Data = categoryRes;
+            response.Message = "Add category successfully.";
+            return Ok(response);
         }
 
         [HttpPut("category/{categoryId}")]
-        public ActionResult<CategoryDto> UpdateCategory(int categoryId, [FromBody] CategoryDto category)
+        public ActionResult UpdateCategory(int categoryId, [FromBody] CategoryDto category)
         {
-            var accountId = 2;// Only admin have permission to add product
+            var accountId = 2;
+            var response = new Response<CategoryDto>();
             var categoryRes = _categoryService.UpdateCategory(accountId, categoryId, category);
 
             if (categoryRes == null)
             {
-                return null;
+                response.Code = 404;
+                response.IsSuccess = false;
+                response.Message = "Category is not exist.";
+                return NotFound(response);
             }
-            return categoryRes;
+            
+            response.Data = categoryRes;
+            response.Message = "Update Category successfully.";
+            return Ok(response);
         }
 
         [HttpDelete("category/{categoryId}")]
-        public bool DeleteCategory(int categoryId)
+        public ActionResult DeleteCategory(int categoryId)
         {
             var accountId = 2;// Only admin have permission to add product
-
+            var response = new Response<string>("");
             var categoryRes = _categoryService.DeleteCategory(accountId, categoryId);
-
-            return categoryRes;
+            if(categoryRes == false)
+            {
+                response.Code = 404; 
+                response.IsSuccess = false;
+                response.Message = "Category is not exist.";
+                return NotFound(response);
+            }
+            response.Message = "Delete category successfull.";
+            return Ok(response);
         }
     }
 }
