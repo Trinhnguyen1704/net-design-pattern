1dbf7cedae5d2d19e5020f11468d965ea94ea6fe
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-06 07:33:08 +0700

    add get user profile function

--- Persistence/Repositories/User/ProfileRepository.cs
+++ Persistence/Repositories/User/ProfileRepository.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using net_design_pattern.Domain.Models;
 using net_design_pattern.Domain.Repositories.User;
+using net_design_pattern.Persistence.Context;
 
 namespace net_design_pattern.Persistence.Repositories.User
 {
     public class ProfileRepository : IProfileRepository
     {
+        public readonly AppDbContext _context;
+        public ProfileRepository(AppDbContext context)
+        {
+            _context = context;
+        }
         public Profile EditProfile(int accountId, Profile profile)
         {
             throw new NotImplementedException();
@@ -16,7 +22,15 @@ namespace net_design_pattern.Persistence.Repositories.User
 
         public Profile GetProfile(int accountId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _context.Profiles.Where(x => x.IsDeleted == false).FirstOrDefault(x => x.AccountId == accountId);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
         }
     }
 }
--- Services/User/ProfileService.cs
+++ Services/User/ProfileService.cs
@@ -2,13 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using net_design_pattern.Domain.Models.DTOs;
+using net_design_pattern.Domain.Repositories.User;
 using net_design_pattern.Domain.Services.User;
 
 namespace net_design_pattern.Services.User
 {
     public class ProfileService : IProfileService
     {
+        public IProfileRepository _profileRepository;
+        public IMapper _mapper;
+        public ProfileService(IProfileRepository profileRepository, IMapper mapper)
+        {
+            _profileRepository = profileRepository;
+            _mapper = mapper;
+        }
         public ProductDto EditProfile(int accountId, ProfileDto profile)
         {
             throw new NotImplementedException();
@@ -16,7 +25,12 @@ namespace net_design_pattern.Services.User
 
         public ProfileDto GetProfile(int accountId)
         {
-            throw new NotImplementedException();
+            var profileRes = _profileRepository.GetProfile(accountId);
+            if(profileRes == null)
+            {
+                return null;
+            }
+            return _mapper.Map<ProfileDto>(profileRes);
         }
     }
 }
