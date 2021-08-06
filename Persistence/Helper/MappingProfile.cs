8cc76f3e3b33d23451b10927726b4c086d14a72c
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-06 07:00:11 +0700

    mapping profile and profile dto

--- Persistence/Helper/MappingProfile.cs
+++ Persistence/Helper/MappingProfile.cs
@@ -21,6 +21,16 @@ namespace net_design_pattern.Persistence.Helper
             ));
             CreateMap<Category, CategoryDto>();
             CreateMap<CategoryDto, Category>();
+
+            CreateMap<Domain.Models.Profile, ProfileDto>()
+            .ForMember(dest => dest.Gender, act => act.MapFrom(
+                src => (src.Gender == 0 ? "Female" : src.Gender == 1 ? "Male" : src.Gender == 2 ? "Other" : null)
+            ));
+
+            CreateMap<ProfileDto, Domain.Models.Profile>()
+            .ForMember(dest => dest.Gender, act => act.MapFrom(
+                src => src.Gender.ToLower().Equals("male") ? 1 : src.Gender.ToLower().Equals("female") ? 0 : src.Gender.ToLower().Equals("other")? 2 : -1
+            ));
         } 
             
     }
