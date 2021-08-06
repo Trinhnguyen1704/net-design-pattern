6887f5dfec14140ed879b920d2adb248ebfaf795
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-06 09:30:27 +0700

    remove dbset for table order

--- Persistence/Context/AppDbContext.cs
+++ Persistence/Context/AppDbContext.cs
@@ -17,17 +17,12 @@ namespace net_design_pattern.Persistence.Context
          public DbSet<AccountHasRole> AccountHasRoles {get; set;}
         public DbSet<Product> Products {get; set;}
         public DbSet<Category> Categories {get; set;}
-        public DbSet<Order> Orders {get; set;}
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Account>()
             .HasMany(x => x.AccountHasRoles)
             .WithOne(x => x.Account);
-
-            builder.Entity<Account>()
-            .HasMany(x => x.Orders)
-            .WithOne(x => x.Account);
-
+            
             builder.Entity<Account>()
             .HasOne(x => x.Profile)
             .WithOne(x => x.Account);
@@ -39,11 +34,6 @@ namespace net_design_pattern.Persistence.Context
             builder.Entity<Product>()
             .HasOne(x => x.Category)
             .WithMany(x => x.Products);
-
-            builder.Entity<Product>()
-            .HasMany(x => x.Orders)
-            .WithMany(x => x.Products);
-            
         }
     }
 }
