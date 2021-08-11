e730f515ff9a97da3358e73e2596e83d0f5c9ee0
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-11 14:51:43 +0700

    add register response

---
+++ Domain/Models/Authorization/RegisterResponse.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_design_pattern.Domain.Models.Authorization
+{
+    public class RegisterResponse
+    {
+        public RegisterResponse()
+        {
+            IsSuccess = true;
+        }
+        public int AccountId {get; set;}
+        public bool IsSuccess {get; set;}
+        public string Error {get; set;}
+    }
+}
