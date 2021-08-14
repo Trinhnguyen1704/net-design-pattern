074f78ed91771b1f8f9aeb8ba1c8f8a31d69add2
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-14 08:42:54 +0700

    add  unit test delete catefory fail with false role

--- UnitTesting/CategoryServiceTesting.cs
+++ UnitTesting/CategoryServiceTesting.cs
@@ -98,6 +98,29 @@ namespace net_design_pattern.Tests
             Assert.NotNull(result);
             result.Id.Should().Equals(expectedItem.Id);
         }
+
+        //test delete category item with false role
+        [Fact]
+        public void CategoryService_DeleteByIdWithFalseRole_Test()
+        {
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            var mapper = mockMapper.CreateMapper();
+            _categoryService = new CategoryService(_categoryRepository.Object,_roleRepository.Object, mapper);
+            var accountId = 2;
+            int categoryId = 1;
+            //arrange
+            _categoryRepository.Setup(c => c.DeleteCategory(It.IsAny<int>())).Returns(true);
+            _roleRepository.Setup(r => r.CheckRole(It.IsAny<int>())).Returns(false);
+            //act
+            var result = _categoryService.DeleteCategory(accountId, categoryId);
+
+            //assert
+            result.Should().Equals(false);
+        }
+        
         // private readonly Mock<ICategoryService> categoryService = new();
         // private readonly Mock<IRoleRepository> roleRepository = new();
         // private readonly IMapper _mapper;
