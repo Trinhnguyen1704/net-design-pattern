af886456ebc9448908f7c4b383177765b991f09a
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-15 10:34:57 +0700

    add unit test get all products

---
+++ UnitTesting/RepositoryTesting/ProductRepositoryTesting.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using net_design_pattern.Domain.Models;
+using net_design_pattern.Domain.Repositories.Common;
+using net_design_pattern.Persistence.Context;
+using net_design_pattern.Persistence.Repositories.Common;
+using Xunit;
+
+namespace net_design_pattern.UnitTesting.RepositoryTesting
+{
+    public class ProductRepositoryTesting
+    {
+        IProductRepository productRepository;
+
+        //get all product test
+         [Fact]
+        public void ProductRepository_GetAll_Test()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "phonestore")
+            .Options;
+            using (var context = new AppDbContext(options))
+            {
+                context.Categories.Add(new Category
+                {
+                    Id = 1,
+                    Name = "IPhone"
+                });
+                context.Products.Add(new Product
+                {
+                    Id = 1,
+                    Name = "IPhone 9",
+                    Price = 120,
+                    NumInStock = 10,
+                    Description = "This is Iphone 9",
+                    IsAvailable = 1,
+                    CategoryId = 1
+                });
+                context.Products.Add(new Product
+                {
+                    Id = 2,
+                    Name = "IPhone 12",
+                    Price = 140,
+                    NumInStock = 20,
+                    Description = "This is Iphone 12",
+                    IsAvailable = 1,
+                    CategoryId = 1
+                });
+                context.SaveChanges();
+            }
+            using (var context = new AppDbContext(options))
+            {
+                productRepository = new ProductRepository(context);
+                //Act
+                var list = productRepository.GetProducts().ToList();
+                //Assert
+                Assert.NotNull(list);
+                list.Should().HaveCount(2);
+            }
+        }
+    }
+}
