bcb9d3e431608b76b34a49f851a4a1c7cb839a4c
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-15 14:49:14 +0700

    add unit test get account roles

---
+++ UnitTesting/RepositoryTesting/RoleRepositoryTesting.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using net_design_pattern.Domain.Models;
+using net_design_pattern.Domain.Repositories.Authorization;
+using net_design_pattern.Persistence.Context;
+using net_design_pattern.Persistence.Helper;
+using net_design_pattern.Persistence.Repositories.Authorization;
+using Xunit;
+
+namespace net_design_pattern.UnitTesting.RepositoryTesting
+{
+    public class RoleRepositoryTesting
+    {
+        IRoleRepository roleRepository;
+        //Get account roles test
+        [Fact]
+        public void ProfileRepository_GetRoles_Test()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "phonestore")
+            .Options;
+            using (var context = new AppDbContext(options))
+            {
+                context.Roles.Add(new Role
+                {
+                    Id = 1,
+                    Name = "ADMIN",
+                });
+                context.Roles.Add(new Role
+                {
+                    Id = 2,
+                    Name = "USER",
+                });
+                context.Accounts.Add(new Account
+                {
+                    Id = 1,
+                    Email = "trinh@gmail.com",
+                    Password = "123",
+                });
+                context.AccountHasRoles.Add(new AccountHasRole
+                {
+                    Id =1,
+                    AccountId = 1,
+                    RoleId = 1
+                });
+                 context.AccountHasRoles.Add(new AccountHasRole
+                {
+                    Id =2,
+                    AccountId = 1,
+                    RoleId = 2
+                });
+
+                context.SaveChanges();
+            }
+            using (var context = new AppDbContext(options))
+            {
+                var mockMapper = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile(new MappingProfile());
+                });
+                
+                var mapper = mockMapper.CreateMapper();
+                roleRepository = new RoleRepository(context, mapper);
+                //Act
+                var result = roleRepository.GetRoles(1).ToList();
+                //Assert
+                Assert.NotNull(result);
+                result.Should().HaveCount(2);
+            }
+        }
+    }
+}
