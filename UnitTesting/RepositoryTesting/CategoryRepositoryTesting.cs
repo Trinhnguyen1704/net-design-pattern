521a03ac95dfe1f2a6b5e1871432e14c18716c2f
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-15 09:16:32 +0700

    add unit test get category by id

--- UnitTesting/RepositoryTesting/CategoryRepositoryTesting.cs
+++ UnitTesting/RepositoryTesting/CategoryRepositoryTesting.cs
@@ -19,6 +19,7 @@ namespace net_design_pattern.UnitTesting.RepositoryTesting
         [Fact]
         public void CategoryRepository_GetAll_Test()
         {
+            //Arrange
             var options = new DbContextOptionsBuilder<AppDbContext>()
             .UseInMemoryDatabase(databaseName: "phonestore")
             .Options;
@@ -34,9 +35,43 @@ namespace net_design_pattern.UnitTesting.RepositoryTesting
             using (var context = new AppDbContext(options))
             {
                 categoryRepository = new CategoryRepository(context);
+                //Act
                 var list = categoryRepository.GetCategories().ToList();
+                //Assert
                 Assert.NotNull(list);
                 list.Should().HaveCount(1);
+            }
+        }
+         [Fact]
+        public void CategoryRepository_GetCategoryById_Test()
+        {
+            //Arange
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "phonestore")
+            .Options;
+            using (var context = new AppDbContext(options))
+            {
+                context.Categories.Add(new Category
+                {
+                    Id = 1,
+                    Name = "IPad"
+                });
+                context.Categories.Add(new Category
+                {
+                    Id = 2,
+                    Name = "IPhone"
+                });
+                context.SaveChanges();
+            }
+            using (var context = new AppDbContext(options))
+            {
+                categoryRepository = new CategoryRepository(context);
+                //Act
+                var category = categoryRepository.GetCategoryById(1);
+                //Assert
+                Assert.NotNull(category);
+                category.Id.Should().Equals(1);
+                category.Name.Should().Equals("IPhone");
             }
         }
     }
