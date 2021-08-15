1ac7fe93797e779d9aeb1ab342dca4b940a2f791
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-08-15 11:11:56 +0700

    add unit test ađ new product

--- UnitTesting/RepositoryTesting/ProductRepositoryTesting.cs
+++ UnitTesting/RepositoryTesting/ProductRepositoryTesting.cs
@@ -63,8 +63,8 @@ namespace net_design_pattern.UnitTesting.RepositoryTesting
                 list.Should().HaveCount(2);
             }
         }
-        //get all product test
-         [Fact]
+        //get product by id test
+        [Fact]
         public void ProductRepository_GetProductById_Test()
         {
             //Arrange
@@ -108,6 +108,53 @@ namespace net_design_pattern.UnitTesting.RepositoryTesting
                 //Assert
                 Assert.NotNull(product);
                 product.Id.Should().Equals(1);
+            }
+        }
+        //add new product test
+        [Fact]
+        public void ProductRepository_AddProduct_Test()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "phonestore")
+            .Options;
+            using (var context = new AppDbContext(options))
+            {
+                context.Categories.Add(new Category
+                {
+                    Id = 1,
+                    Name = "IPhone"
+                });
+                context.Products.Add(new Product
+                {
+                    Id = 1,
+                    Name = "IPhone 9",
+                    Price = 120,
+                    NumInStock = 10,
+                    Description = "This is Iphone 9",
+                    IsAvailable = 1,
+                    CategoryId = 1
+                });
+                context.SaveChanges();
+            }
+            using (var context = new AppDbContext(options))
+            {
+                productRepository = new ProductRepository(context);
+                var product = new Product
+                {
+                    Name = "IPhone 12",
+                    Price = 140,
+                    NumInStock = 20,
+                    Description = "This is Iphone 12",
+                    IsAvailable = 1,
+                    CategoryId = 1
+                };
+                //Act
+                var result = productRepository.AddProduct(product);
+                //Assert
+                Assert.NotNull(result);
+                result.Id.Should().Equals(2);
+                result.Name.Should().Equals("IPhone 12");
             }
         }
     }
